b773e7798706799000bf3af811c01ef8038f1595
diego-val-vel <diego.val.vel@gmail.com>
2025-11-25 18:17:00 -0600

    Se hacen ajustes a la parte 2 de la practica 3

--- src/main/java/com/segurosargos/hotelbook/dto/RoomUpdateRequestDto.java
+++ src/main/java/com/segurosargos/hotelbook/dto/RoomUpdateRequestDto.java
@@ -7,8 +7,10 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /*
  * Datos de entrada para actualizar una habitación existente.
@@ -17,6 +19,8 @@ import lombok.Data;
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoomUpdateRequestDto {
 
     /*
